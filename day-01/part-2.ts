e92c56d3381660949d2b56f56cacf8509d1b4af5
brandongasser <bgasser12@gmail.com>
2024-12-01 12:43:16 -0600

    Optimize Day 1 Part 2

--- day-01/part-2.ts
+++ day-01/part-2.ts
@@ -1,9 +1,23 @@
 import { count } from "../utils/utils";
 
 export function part2(input: number[][]): number {
+    const rightCounts = getCounts(input[1]);
     let result = 0;
     for (const num of input[0]) {
-        result += num * count(input[1], x => x === num);
+        if (rightCounts[num]) {
+            result += num * rightCounts[num];
+        }
     }
     return result;
+}
+
+function getCounts(nums: number[]): object {
+    const memo = {};
+    for (const num of nums) {
+        if (!memo[num]) {
+            memo[num] = 0;
+        }
+        memo[num]++;
+    }
+    return memo;
 }
