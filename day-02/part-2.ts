8e78d17f988f3ee57d930c800faab71b303f2044
brandongasser <bgasser12@gmail.com>
2024-12-02 01:01:39 -0600

    Day 2 Part 2 Solved

--- day-02/part-2.ts
+++ day-02/part-2.ts
@@ -1,3 +1,45 @@
-export function part2(input: any): number {
-    throw new Error('Not Implemented');
+import { count, fold, zipWith } from "../utils/utils";
+
+export function part2(input: number[][]): number {
+    return fold(input, (acc, report) => isSafe(report) ? acc + 1 : acc, 0);
+}
+
+export function test(differences: number[]): boolean {
+    return !differences.includes(0) && !differences.find(x => Math.abs(x) > 3) && !(differences.find(x => x > 0) && differences.find(x => x < 0));
+}
+
+export function isSafe(report: number[]): boolean {
+    const differences = zipWith(report, report.slice(1), (a, b) => b - a);
+
+    if (test(differences)) {
+        return true;
+    }
+
+    let problemIndex = -1;
+    if (differences.includes(0)) {
+        problemIndex = differences.indexOf(0);
+    }
+    if (differences.find(x => Math.abs(x) > 3)) {
+        problemIndex = differences.findIndex(x => Math.abs(x) > 3);
+    }
+    if (differences.find(x => x > 0) && differences.find(x => x < 0)) {
+        if (count(differences, x => x < 0) < count(differences, x => x > 0)) {
+            problemIndex = differences.findIndex(x => x < 0);
+        } else {
+            problemIndex = differences.findIndex(x => x > 0);
+        }
+    }
+
+    return test(removeReading(differences, problemIndex)) || test(removeReading(differences, problemIndex + 1));
+}
+
+export function removeReading(differences: number[], differenceIndex: number): number[] {
+    if (differenceIndex === 0) {
+        return differences.slice(1);
+    }
+    return [
+        ...differences.slice(0, differenceIndex - 1),
+        differences[differenceIndex - 1] + differences[differenceIndex],
+        ...differences.slice(differenceIndex + 1)
+    ];
 }
--- day-02/spec/part-2.spec.ts
+++ day-02/spec/part-2.spec.ts
@@ -1,2 +1,92 @@
 import * as Part2 from '../part-2';
 
+const testInput = [
+    [ 7, 6, 4, 2, 1 ],
+    [ 1, 2, 7, 8, 9 ],
+    [ 9, 7, 6, 2, 1 ],
+    [ 1, 3, 2, 4, 5 ],
+    [ 8, 6, 4, 4, 1 ],
+    [ 1, 3, 6, 7, 9 ]
+];
+
+describe('Day 2 Part 2', () => {
+    describe('part2', () => {
+        it('should return the correct answer', () => {
+            const result = Part2.part2(testInput);
+
+            expect(result).toEqual(4);
+        });
+    });
+
+    describe('test', () => {
+        it('should return true for safe differences', () => {
+            const result = Part2.test([ -1, -2, -2, -1 ]);
+
+            expect(result).toBeTrue();
+        });
+
+        it('should return false when there is a 0 difference', () => {
+            const result = Part2.test([ 1, 0, 2, 1 ]);
+
+            expect(result).toBeFalse();
+        });
+
+        it('should return false when there is a difference > 3', () => {
+            const result = Part2.test([ 1, 2, 4, 1 ]);
+
+            expect(result).toBeFalse();
+        });
+
+        it('should return false when there is a difference < -3', () => {
+            const result = Part2.test([ -1, -4, -2, -1 ]);
+
+            expect(result).toBeFalse();
+        });
+
+        it('should return false when there is a mix of increases and decreases', () => {
+            const result = Part2.test([ 1, -2, 2, 1 ]);
+
+            expect(result).toBeFalse();
+        });
+    });
+
+    describe('isSafe', () => {
+        it('should return true for decreasing reports', () => {
+            const result = Part2.isSafe([ 7, 6, 4, 2, 1 ]);
+
+            expect(result).toBeTrue();
+        });
+
+        it('should return true for increasing reports', () => {
+            const result = Part2.isSafe([ 1, 3, 6, 7, 9 ]);
+
+            expect(result).toBeTrue();
+        });
+
+        it('should return true for dampened errors', () => {
+            const result = Part2.isSafe([ 1, 3, 2, 4, 5 ]);
+
+            expect(result).toBeTrue();
+        });
+
+        it('should return false for undampened errors', () => {
+            const result = Part2.isSafe([ 1, 2, 7, 8, 9 ]);
+            
+            expect(result).toBeFalse();
+        });
+    });
+
+    describe('removeReading', () => {
+        it('should remove a reading', () => {
+            const result = Part2.removeReading([ 2, -1, 2, 1 ], 1);
+            
+            expect(result).toEqual([ 1, 2, 1 ]);
+        });
+
+        it('should remove a reading from the beginning', () => {
+            const result = Part2.removeReading([ 2, -1, 2, 1 ], 0);
+
+            expect(result).toEqual([ -1, 2, 1 ]);
+        });
+    });
+});
