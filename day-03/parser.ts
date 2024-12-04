ceb1b350c80a697a25bc0470d67d49a52cdbb000
brandongasser <bgasser12@gmail.com>
2024-12-03 18:35:28 -0600

    Clean up day 3 parser

--- day-03/parser.ts
+++ day-03/parser.ts
@@ -1,50 +1,14 @@
 import { Instruction, Program } from "./common";
 
 export function parse(input: string, part: number): Program {
-    if (part === 1) {
-        return part1(input);
-    } else {
-        return part2(input);
-    }
-}
+    const matches = input.match(/mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)/g)!;
 
-function part1(input: string): Program {
-    const matches = input.matchAll(/mul\([0-9]{1,3},[0-9]{1,3}\)/g);
-
-    const result: Program = [];
-
-    let current = matches.next();
-    while (!current.done) {
-        const parts = current.value[0].split(',');
-        const instruction: Instruction = [ +parts[0].slice(4), +parts[1].slice(0, parts[1].length - 1) ];
-
-        result.push(instruction);
-
-        current = matches.next();
-    }
-
-    return result;
-}
-
-function part2(input: string): Program {
-    const matches = input.matchAll(/(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\))|(don't\(\))/g);
-
-    const result: Program = [];
-    let current = matches.next();
-    while(!current.done) {
-        if (current.value[0] === 'do()') {
-            result.push('do');
-        } else if (current.value[0] === 'don\'t()') {
-            result.push('dont');
-        } else {
-            const parts = current.value[0].split(',');
-            const instruction: Instruction = [ +parts[0].slice(4), +parts[1].slice(0, parts[1].length - 1) ];
-
-            result.push(instruction);
+    return matches.map(match => {
+        if (match === 'do()') {
+            return 'do';
+        } else if (match === 'don\'t()') {
+            return 'dont';
         }
-
-        current = matches.next();
-    }
-
-    return result;
+        return match.match(/[0-9]+/g)!.map(m => +m);
+    }) as Program;
 }
