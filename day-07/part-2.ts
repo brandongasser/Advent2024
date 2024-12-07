e5309c62f459d04f279cba7ca0a9a126d7a3fabc
brandongasser <bgasser12@gmail.com>
2024-12-07 00:07:48 -0600

    Day 7 Part 2 Solved

--- day-07/part-2.ts
+++ day-07/part-2.ts
@@ -1,3 +1,31 @@
-export function part2(input: any): number {
-    throw new Error('Not Implemented');
+export function part2(input: { result: number, nums: number[] }[]): number {
+    let answer = 0;
+    for (const equation of input) {
+        if (isPossible(equation)) {
+            answer += equation.result;
+        }
+    }
+    return answer;
+}
+
+export function isPossible(equation: { result: number, nums: number[] }): boolean {
+    const cases = Math.pow(3, equation.nums.length - 1);
+
+    for (let i = 0; i < cases; i++) {
+        const flags = i.toString(3).padStart(equation.nums.length - 1, '0');
+        let result = equation.nums[0];
+        for (let j = 1; j < equation.nums.length; j++) {
+            if (flags[j - 1] === '0') {
+                result += equation.nums[j];
+            } else if (flags[j - 1] === '1') {
+                result *= equation.nums[j];
+            } else {
+                result = +(result.toString() + equation.nums[j]);
+            }
+        }
+        if (result === equation.result) {
+            return true;
+        }
+    }
+    return false;
 }
--- day-07/spec/part-2.spec.ts
+++ day-07/spec/part-2.spec.ts
@@ -1,2 +1,37 @@
 import * as Part2 from '../part-2';
 
+describe('Day 7 Part 2', () => {
+    describe('part2', () => {
+        it('should return the correct answer', () => {
+            const input = [
+                { result: 190, nums: [ 10, 19 ] },
+                { result: 3267, nums: [ 81, 40, 27 ] },
+                { result: 83, nums: [ 17, 5 ] },
+                { result: 156, nums: [ 15, 6 ] },
+                { result: 7290, nums: [ 6, 8, 6, 15 ] },
+                { result: 161011, nums: [ 16, 10, 13 ] },
+                { result: 192, nums: [ 17, 8, 14 ] },
+                { result: 21037, nums: [ 9, 7, 18, 13 ] },
+                { result: 292, nums: [ 11, 6, 16, 20 ] }
+            ];
+
+            const result = Part2.part2(input);
+
+            expect(result).toEqual(11387);
+        });
+    });
+
+    describe('isPossible', () => {
+        it('should return true for a possible equation', () => {
+            const result = Part2.isPossible({ result: 7290, nums: [ 6, 8, 6, 15 ] });
+
+            expect(result).toBeTrue();
+        });
+
+        it('should return false for an impossible equation', () => {
+            const result = Part2.isPossible({ result: 21037, nums: [ 9, 7, 18, 13 ] });
+
+            expect(result).toBeFalse();
+        });
+    });
+});
